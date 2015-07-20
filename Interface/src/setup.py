7aa86b21ad311d2280cb4c1646cf018df7389733
Josh <jeremie.meurisse@gmail.com>
2015-07-20 10:34:03 -0700

    setup 2

--- Interface/src/setup.py
+++ Interface/src/setup.py
@@ -1,10 +1,25 @@
 import sys
 from cx_Freeze import setup, Executable
+##
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from tkinter.constants import DISABLED
+from matplotlib import style
+import numpy as np
+import time
+import tkinter as tk
+from tkinter import ttk
+
+
 
 
 # Dependencies are automatically detected, but it might need fine tuning.
 includefiles = ['ReadMe.txt','sampleTest.txt']
-build_exe_options = { "includes": ["tkinter","matplotlib","numpy","time"]}
+#packages=['matplotlib.backends.backend_tkagg']
+build_exe_options = { "excludes": ["dateutil","pytz","six","pyparsing","time","tkinter","matplotlib","numpy","time"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -15,5 +30,5 @@ if sys.platform == "win32":
 setup(  name = "interface",
         version = "0.1",
         description = "My GUI application!",
-        options = {"build.exe": build_exe_options },
+        options = {"build.exe":build_exe_options},
         executables = [Executable("interface.py", base=base)])
